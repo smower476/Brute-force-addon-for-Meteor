d618b453b46547ec11865dab09e2c6d5d99d86a2
chicken <121474491+smower476@users.noreply.github.com>
2023-05-25 04:57:58 +0300

    added onActivate() func

--- src/main/java/com/example/addon/modules/Bruteforce.java
+++ src/main/java/com/example/addon/modules/Bruteforce.java
@@ -2,9 +2,14 @@ package com.example.addon.modules;
 
 import com.example.addon.Addon;
 import meteordevelopment.meteorclient.systems.modules.Module;
+import meteordevelopment.meteorclient.utils.player.ChatUtils;
 
 public class Bruteforce extends Module {
     public Bruteforce() {
         super(Addon.CATEGORY, "Brute-force", "Brute-force.");
+    }
+    @Override
+    public void onActivate() {
+        ChatUtils.sendPlayerMsg("I am a very bad person!");
     }
 }
