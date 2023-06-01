ce76e879c13de55805ac2228e2636bc1ce397dbc
chicken <121474491+smower476@users.noreply.github.com>
2023-06-01 05:56:04 +0300

    version w/o spam proc

--- src/main/java/com/example/addon/modules/dupe.java
+++ src/main/java/com/example/addon/modules/dupe.java
@@ -9,6 +9,8 @@ import meteordevelopment.meteorclient.systems.modules.Module;
 import meteordevelopment.meteorclient.settings.*;
 import meteordevelopment.meteorclient.utils.player.ChatUtils;
 import meteordevelopment.orbit.EventHandler;
+import net.minecraft.network.packet.s2c.play.DisconnectS2CPacket;
+import net.minecraft.text.Text;
 
 import java.util.List;
 
@@ -16,12 +18,14 @@ public class dupe extends Module {
     private final SettingGroup sgGeneral = settings.getDefaultGroup();
     private final Setting<Integer> delay = sgGeneral.add(new IntSetting.Builder()
         .name("delay")
-        .description("The delay between specified messages in ticks.")
-        .defaultValue(20)
+        .description("The delay before disconnect. 20 Ticks = 1 sec.")
+        .defaultValue(300)
         .min(0)
-        .sliderMax(200)
+        .sliderMax(1200)
         .build()
     );
+
+
 
     public dupe() {
         super(Addon.CATEGORY, "dupe", "dupe.");
@@ -36,22 +40,26 @@ public class dupe extends Module {
     }
     @EventHandler
     private void onTick(TickEvent.Post event) {
-        TickCounter+=1;
-        if (TickCounter % 20 ==0){ChatUtils.sendPlayerMsg("Tick!"); ChatUtils.sendPlayerMsg(String.valueOf(a));}
-        if (a == true && TickCounter % 20 == 0){ChatUtils.sendPlayerMsg(String.valueOf(TickCounter)); ChatUtils.sendPlayerMsg(String.valueOf(delay));}
+        if (a == true) {TickCounter += 1;};
+        if (TickCounter % 20 == 0 && TickCounter % 20 != 0){ info(String.valueOf(TickCounter) + "ticks have passed");}
+        //if (TickCounter % 20 ==0){ChatUtils.sendPlayerMsg("Tick!"); ChatUtils.sendPlayerMsg(String.valueOf(a));}
+        //if (a == true && TickCounter % 20 == 0){ChatUtils.sendPlayerMsg(String.valueOf(TickCounter)); ChatUtils.sendPlayerMsg(String.valueOf(delay));}
         //meteor client
         //mc.player.networkHandler.onDisconnect(new DisconnectS2CPacket(Text.literal("[AutoLog] A non-trusted player appeared in your render distance.")));
+        if (a == true && TickCounter  == delay.get()){ChatUtils.sendPlayerMsg(String.valueOf(TickCounter)); mc.player.networkHandler.onDisconnect(new DisconnectS2CPacket(Text.literal("dupe."))); a = false;}
+        if (TickCounter  >= delay.get()){ChatUtils.sendPlayerMsg(String.valueOf(TickCounter)); mc.player.networkHandler.onDisconnect(new DisconnectS2CPacket(Text.literal("ERROR!"))); a = false;}
     }
     @EventHandler
     private void onDamage(DamageEvent event) {
-        if (event.entity.getUuid() == null) return;
-        if (!event.entity.getUuid().equals(mc.player.getUuid())) return;
+        //if (event.entity.getUuid() == null) return;
+        //if (!event.entity.getUuid().equals(mc.player.getUuid())) return;
 
         a = true;
     }
 
     @EventHandler
     private void onGameLeft(GameLeftEvent event) {
+        a = false;
         toggle();
     }
 }
